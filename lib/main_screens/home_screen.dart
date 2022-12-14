b4015a934ac7d96e05358286f0474018cda2426d
altrono <hbotema16@gmail.com>
2022-12-15 01:12:20 +0200

    Added Tab using DefaultTabController

--- lib/main_screens/home_screen.dart
+++ lib/main_screens/home_screen.dart
@@ -11,11 +11,61 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.white,
-        title: const CupertinoSearchTextField(),
+    return DefaultTabController(
+      length: 9,
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.white,
+          title: const CupertinoSearchTextField(),
+          bottom:const TabBar(
+            isScrollable: true,
+            indicatorColor: Colors.yellow,
+            indicatorWeight: 8,
+            tabs: [
+              RepeatedTab(name: 'Men',),
+              RepeatedTab(name: 'Woman',),
+              RepeatedTab(name: 'Shoes',),
+              RepeatedTab(name: 'Bags',),
+              RepeatedTab(name: 'Electronics',),
+              RepeatedTab(name: 'Accessories',),
+              RepeatedTab(name: 'Home & Garden',),
+              RepeatedTab(name: 'Kids',),
+              RepeatedTab(name: 'Beauty',),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            Center(child: Text('men screen'),),
+            Center(child: Text('women screen'),),
+            Center(child: Text('shoes screen'),),
+            Center(child: Text('Bag screen'),),
+            Center(child: Text('Electronics screen'),),
+            Center(child: Text('Accessories screen'),),
+            Center(child: Text('Home & Garden screen'),),
+            Center(child: Text('Kids screen'),),
+            Center(child: Text('Beauty screen'),),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class RepeatedTab extends StatelessWidget {
+  final String name;
+  const RepeatedTab({
+    super.key,
+    required this.name
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Tab(
+      child: Text(
+        name,
+        style: TextStyle(color: Colors.grey.shade600),
       ),
     );
   }
