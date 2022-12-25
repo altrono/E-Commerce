37856b508cc3bff05c767e931723b712124a8bd9
altrono <hbotema16@gmail.com>
2022-12-25 21:31:56 +0200

    added more functionalities to side navigator

--- lib/main_screens/category_screen.dart
+++ lib/main_screens/category_screen.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:zando/main_screens/home_screen.dart';
 
+List<ItemsData> items = [
+  ItemsData(label: 'men'),
+  ItemsData(label: 'women'),
+  ItemsData(label: 'shoes'),
+  ItemsData(label: 'bags'),
+  ItemsData(label: 'electronics'),
+  ItemsData(label: 'accessories'),
+  ItemsData(label: 'home & garden'),
+  ItemsData(label: 'kids'),
+  ItemsData(label: 'beauty'),
+];
+
 class CategoryScreen extends StatefulWidget {
   const CategoryScreen({Key? key}) : super(key: key);
 
@@ -16,7 +28,7 @@ class _CategoryScreenState extends State<CategoryScreen> {
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Colors.white,
-        title: FakeSearch(),
+        title: const FakeSearch(),
       ),
       body: Stack(
         children: [
@@ -36,10 +48,30 @@ class _CategoryScreenState extends State<CategoryScreen> {
   }
 
   Widget sideNavigator (Size size){
-    return Container(
+    return SizedBox(
       height: size.height * 0.8,
       width: size.width * 0.2,
-      color: Colors.grey.shade300,
+      child: ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: (){
+              for (var element in items) {
+                element.isSelected = false;
+              }
+              setState(() {
+                  items[index].isSelected = true;
+              });
+            },
+            child: Container(
+                color:items[index].isSelected ? Colors.white : Colors.grey.shade300,
+                height: 100,
+                child: Center(
+                    child: Text(items[index].label)),
+            ),
+          );
+        },
+      ),
     );
   }
 
@@ -51,4 +83,11 @@ class _CategoryScreenState extends State<CategoryScreen> {
     );
   }
 
+}
+
+
+class ItemsData {
+  String label;
+  bool isSelected;
+  ItemsData({required this.label, this.isSelected = false});
 }
