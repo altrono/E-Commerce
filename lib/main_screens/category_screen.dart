a8c3815364da45de427b28491e8f2128fc90f808
altrono <hbotema16@gmail.com>
2022-12-25 19:35:49 +0200

    modularize categoryscreen

--- lib/main_screens/category_screen.dart
+++ lib/main_screens/category_screen.dart
@@ -11,13 +11,44 @@ class CategoryScreen extends StatefulWidget {
 class _CategoryScreenState extends State<CategoryScreen> {
   @override
   Widget build(BuildContext context) {
+    var size =  MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Colors.white,
         title: FakeSearch(),
       ),
-      body: Center(child: Text('Caetgory body'),),
+      body: Stack(
+        children: [
+          Positioned(
+              bottom: 0,
+              left: 0,
+              child: sideNavigator(size),
+          ),
+          Positioned(
+            bottom: 0,
+            right: 0,
+            child: categoryView(size),
+          ),
+        ],
+      ),
     );
   }
+
+  Widget sideNavigator (Size size){
+    return Container(
+      height: size.height * 0.8,
+      width: size.width * 0.2,
+      color: Colors.grey.shade300,
+    );
+  }
+
+  Widget categoryView (Size size){
+    return Container(
+      height: size.height * 0.8,
+      width: size.width * 0.8,
+      color: Colors.white,
+    );
+  }
+
 }
