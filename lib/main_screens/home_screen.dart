8b089655a79102fcdf30e54198080d5e445a43e8
altrono <hbotema16@gmail.com>
2022-12-25 18:50:55 +0200

    extracted inkwell widget from homescreen to create a fakesearch widget for reusabilty

--- lib/main_screens/home_screen.dart
+++ lib/main_screens/home_screen.dart
@@ -18,38 +18,7 @@ class _HomeScreenState extends State<HomeScreen> {
         appBar: AppBar(
           elevation: 0,
           backgroundColor: Colors.white,
-          title: InkWell(
-            onTap: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context) => SearchScreen()));
-            },
-            child: Container(
-              height: 35,
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.yellow, width: 1.4),
-                borderRadius:const BorderRadius.all(Radius.circular(25)),
-              ),
-              child: Row(
-                children:  [
-                  const Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 10.0),
-                    child: Icon(Icons.search, color: Colors.grey,),
-                  ),
-                  const Text('What are you looking for', style: TextStyle(fontSize: 18, color: Colors.grey),),
-                  const Spacer(),
-                  Container(
-                    height: 32,
-                    width: 75,
-                    decoration: BoxDecoration(
-                      color: Colors.yellow,
-                      borderRadius: BorderRadius.circular(25),
-                    ),
-                    child: const Center(child: Text('Search', style: TextStyle(fontSize: 16, color: Colors.grey),)),
-                  ),
-
-                ],
-              ),
-            ),
-          ),
+          title: FakeSearch(),
           bottom:const TabBar(
             isScrollable: true,
             indicatorColor: Colors.yellow,
@@ -78,6 +47,48 @@ class _HomeScreenState extends State<HomeScreen> {
             Center(child: Text('Home & Garden screen'),),
             Center(child: Text('Kids screen'),),
             Center(child: Text('Beauty screen'),),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class FakeSearch extends StatelessWidget {
+  const FakeSearch({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: (){
+        Navigator.push(context, MaterialPageRoute(builder: (context) => SearchScreen()));
+      },
+      child: Container(
+        height: 35,
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.yellow, width: 1.4),
+          borderRadius:const BorderRadius.all(Radius.circular(25)),
+        ),
+        child: Row(
+          children:  [
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10.0),
+              child: Icon(Icons.search, color: Colors.grey,),
+            ),
+            const Text('What are you looking for', style: TextStyle(fontSize: 18, color: Colors.grey),),
+            const Spacer(),
+            Container(
+              height: 32,
+              width: 75,
+              decoration: BoxDecoration(
+                color: Colors.yellow,
+                borderRadius: BorderRadius.circular(25),
+              ),
+              child: const Center(child: Text('Search', style: TextStyle(fontSize: 16, color: Colors.grey),)),
+            ),
+
           ],
         ),
       ),
