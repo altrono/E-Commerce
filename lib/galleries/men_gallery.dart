58314d45e013e8c961feb60d68e70c7bd829f0a2
altrono <hbotema16@gmail.com>
2023-02-28 12:53:13 +0200

    addded ProductModel widget

--- lib/galleries/men_gallery.dart
+++ lib/galleries/men_gallery.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:staggered_grid_view_flutter/widgets/staggered_grid_view.dart';
 import 'package:staggered_grid_view_flutter/widgets/staggered_tile.dart';
 
+import '../models/product_model.dart';
+
 class MenGalleryScreen extends StatefulWidget {
   const MenGalleryScreen({Key? key}) : super(key: key);
 
@@ -11,17 +13,36 @@ class MenGalleryScreen extends StatefulWidget {
 }
 
 class _MenGalleryScreenState extends State<MenGalleryScreen> {
-  final Stream<QuerySnapshot> _productStream = FirebaseFirestore.instance.collection('products').snapshots();
+  final Stream<QuerySnapshot> _productStream = FirebaseFirestore.instance.collection('products').where(
+    'maincateg',
+    isEqualTo: 'men'
+  ).snapshots();
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<QuerySnapshot>(
         stream: _productStream,
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot){
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
           if (snapshot.hasError) {
-            return Text('Sum went wrong');
+            return const Center(child: Text('Sum went wrong'));
           }
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return Text('Loding');
+            return const Center(child: Text('Loading...'));
+          }
+
+          if (snapshot.data!.docs.isEmpty){
+            return const Center(
+              child: Text(
+                  'This category \nhas no items yet!',
+                  style: TextStyle(
+                    fontSize: 22,
+                    color: Colors.blueGrey,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Acme',
+                    letterSpacing: 1.5
+                  ),
+                  textAlign: TextAlign.center,
+              ),
+            );
           }
           return SingleChildScrollView(
             child: StaggeredGridView.countBuilder(
@@ -30,69 +51,12 @@ class _MenGalleryScreenState extends State<MenGalleryScreen> {
               itemCount: snapshot.data!.docs.length,
                 crossAxisCount: 2,
                 itemBuilder: (context, index){
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(15)
-                      ),
-                      child: Column(
-                        children: [
-                            ClipRRect(
-                              borderRadius: BorderRadius.only(
-                                  topLeft: Radius.circular(15),
-                                  topRight: Radius.circular(15)
-                              ),
-                              child: Container(
-                                constraints: BoxConstraints(maxHeight: 250, minHeight: 100),
-                                child: Image(
-                                  image: NetworkImage(snapshot.data!.docs[index]['proimages'][0]),
-                                ),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Column(
-                                children: [
-                                  Text(
-                                      snapshot.data!.docs[index]['proname'],
-                                      maxLines: 2,
-                                      overflow: TextOverflow.ellipsis,
-                                      style: TextStyle(color: Colors.grey.shade600, fontSize: 16, fontWeight: FontWeight.w600),
-                                  ),
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Text(
-                                        'R ${snapshot.data!.docs[index]['price'].toStringAsFixed(2)}',
-                                        style: TextStyle(
-                                          color: Colors.red,
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.w600,
-                                        ),
-                                      ),
-                                      IconButton(
-                                        onPressed: (){},
-                                        icon: Icon(
-                                          Icons.favorite_border_outlined,
-                                          color: Colors.red,
-                                        ),
-
-                                      ),
-                                    ],
-                                  )
-                                ],
-                              ),
-                            ),
-
-                        ],
-                      ),
-                    ),
-                  );
+                  return ProductModel(products: snapshot.data!.docs[index],);
                 },
                 staggeredTileBuilder: (context) => const StaggeredTile.fit(1)),
           );
         });
   }
 }
+
+
