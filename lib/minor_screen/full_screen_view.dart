2af08348ab7f6cdb7f372531261f4a45ab0b1e6a
altrono <hbotema16@gmail.com>
2023-03-15 11:54:49 +0200

    added FullScreenView widget part 2

--- lib/minor_screen/full_screen_view.dart
+++ lib/minor_screen/full_screen_view.dart
@@ -10,8 +10,12 @@ class FullScreenView extends StatefulWidget {
 }
 
 class _FullScreenViewState extends State<FullScreenView> {
+  final PageController _controller = PageController();
+
+  int index = 0;
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -19,44 +23,69 @@ class _FullScreenViewState extends State<FullScreenView> {
         backgroundColor: Colors.white,
         leading: const AppBarBackButton(),
       ),
-      body: Column(
-        children: [
-          Center(child: Text('1/5', style: TextStyle(letterSpacing: 8, fontSize: 24),)),
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.5, child: PageView(
-            children: List.generate(widget.imagesList.length, (index) {
-              return InteractiveViewer(
-                      transformationController: TransformationController(),
-                      child: Image.network(widget.imagesList[index].toString(),
-                  ),
-              );
-            }),
-          ),
-          ),
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.2,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: widget.imagesList.length,
-                itemBuilder: (context, index) {
-                  return Container(
-                    width: 120,
-                    margin: const EdgeInsets.all(8),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        width: 4,
-                        color: Colors.yellow
-                      ),
-                      borderRadius: BorderRadius.circular(15)
-                    ),
-                      child: ClipRRect(
-                          borderRadius: BorderRadius.circular(10),
-                          child: Image.network(widget.imagesList[index], fit: BoxFit.cover,)));
-
-            }),
-          ),
-        ],
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Center(child: Text('${index + 1}/${widget.imagesList.length}', style: TextStyle(letterSpacing: 8, fontSize: 24),)),
+            SizedBox(
+              height: size.height * 0.5,
+              child: PageView(
+                onPageChanged: (value) {
+                  setState(() {
+                    index = value;
+                  });
+                },
+                controller: _controller,
+                children: images(),
+              ),
+            ),
+            SizedBox(
+              height: size.height * 0.2,
+              child: imageView(),
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+
+  List<Widget> images() {
+    return List.generate(widget.imagesList.length, (index) {
+      return InteractiveViewer(
+        transformationController: TransformationController(),
+        child: Image.network(widget.imagesList[index].toString(),
+        ),
+      );
+    });
+  }
+
+
+  Widget imageView(){
+    return ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: widget.imagesList.length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: (){
+              _controller.jumpToPage(index);
+            },
+            child: Container(
+                width: 120,
+                margin: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                    border: Border.all(
+                        width: 4,
+                        color: Colors.yellow
+                    ),
+                    borderRadius: BorderRadius.circular(15)
+                ),
+                child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.network(widget.imagesList[index], fit: BoxFit.cover,))),
+          );
+
+        });
   }
 }
